ea5d5ae9d72e893399e2e97e10140e243b675424
Danil Zagoskin <z@gosk.in>
2012-07-25 19:01:51 +0400

    stockdb_format: add EUnit tests

--- src/stockdb_format.erl
+++ src/stockdb_format.erl
@@ -88,6 +88,7 @@ decode_delta_field(<<1:1, ValueTail/bitstring>>) ->
 decode_trade(<<1:1, 1:1, Timestamp:62/integer, Price:32/integer, Volume:32/integer, Tail/bitstring>>) ->
   {Timestamp, Price, Volume, Tail}.
 
+
 format_header_value(date, {Y, M, D}) ->
   io_lib:format("~4..0B/~2..0B/~2..0B", [Y, M, D]);
 
@@ -122,3 +123,58 @@ parse_header_value(stock, StockStr) ->
 parse_header_value(_, Value) ->
   Value.
 
+% EUnit tests
+-include_lib("eunit/include/eunit.hrl").
+
+full_md_test() ->
+  Timestamp = 16#138BDF77CBA,
+  BidAsk = [[{1530, 250}, {1520, 111}], [{1673, 15}, {1700, 90}]],
+  Bin = <<16#80000138BDF77CBA:64/integer,
+    1530:32/integer, 250:32/integer,  1520:32/integer, 111:32/integer,
+    1673:32/integer, 15:32/integer,   1700:32/integer, 90:32/integer>>,
+  Tail = <<7, 239, 183, 19>>,
+
+  ?assertEqual(full_md, packet_type(Bin)),
+  ?assertEqual(Bin, encode_full_md(Timestamp, BidAsk)),
+  ?assertEqual({Timestamp, BidAsk, Tail}, decode_full_md(<<Bin/binary, Tail/bitstring>>, 2)).
+
+delta_md_test() ->
+  DTimestamp = 270,
+  DBidAsk = [[{0, -4}, {0, 0}], [{-230, 100}, {-100, -47}]],
+  Bin = <<0:1, 142,2,  0:1, 1:1,124,  0:1, 0:1,  1:1,154,126, 1:1,228,0,  1:1,156,127,  1:1,81,  0:7>>,
+  Tail = <<7, 239, 183, 19>>,
+
+  ?assertEqual(delta_md, packet_type(Bin)),
+  ?assertEqual(Bin, encode_delta_md(DTimestamp, DBidAsk)),
+  ?assertEqual({DTimestamp, DBidAsk, Tail}, decode_delta_md(<<Bin/bitstring, Tail/bitstring>>, 2)).
+
+trade_test() ->
+  Timestamp = 16#138BDF77CBA,
+  Price = 16#DEAD,
+  Volume = 16#BEEF,
+  Bin = <<16#C0000138BDF77CBA:64/integer, Price:32/integer, Volume:32/integer>>,
+  Tail = <<7, 239, 183, 19>>,
+
+  ?assertEqual(trade, packet_type(Bin)),
+  ?assertEqual(Bin, encode_trade(Timestamp, Price, Volume)),
+  ?assertEqual({Timestamp, Price, Volume, Tail}, decode_trade(<<Bin/binary, Tail/binary>>)).
+
+timestamp_test() ->
+  ?assertEqual(16#138BDF77CBA, decode_timestamp(<<16#80000138BDF77CBA:64/integer, 7, 239, 183, 19>>)),
+  ?assertEqual(16#138BDF77CBA, decode_timestamp(<<16#C0000138BDF77CBA:64/integer, 7, 239, 183, 19>>)).
+
+format_header_value_test() ->
+  ?assertEqual("2012/07/19", lists:flatten(format_header_value(date, {2012, 7, 19}))),
+  ?assertEqual("MICEX.URKA", lists:flatten(format_header_value(stock, 'MICEX.URKA'))),
+  ?assertEqual("17", lists:flatten(format_header_value(depth, 17))),
+  ?assertEqual("17", lists:flatten(format_header_value(scale, 17))),
+  ?assertEqual("17", lists:flatten(format_header_value(chunk_size, 17))),
+  ?assertEqual("17", lists:flatten(format_header_value(version, 17))).
+
+parse_header_value_test() ->
+  ?assertEqual({2012, 7, 19}, parse_header_value(date, "2012/07/19")),
+  ?assertEqual('MICEX.URKA', parse_header_value(stock, "MICEX.URKA")),
+  ?assertEqual(17, parse_header_value(depth, "17")),
+  ?assertEqual(17, parse_header_value(scale, "17")),
+  ?assertEqual(17, parse_header_value(chunk_size, "17")),
+  ?assertEqual(17, parse_header_value(version, "17")).
