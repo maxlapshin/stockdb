b1ad17ba37b7f33958750d7158144a77d4941f4b
Danil Zagoskin <z@gosk.in>
2012-08-08 20:44:45 +0400

    stockdb_raw: buffer work: read_event, seek_utc

--- src/stockdb_raw.erl
+++ src/stockdb_raw.erl
@@ -6,7 +6,7 @@
 -include("log.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--export([open/2, read_file/1, file_info/2, append/2, close/1]).
+-export([open/2, restore_state/1, seek_utc/2, read_event/1, read_file/1, file_info/2, append/2, close/1]).
 -export([foldl/3, foldl_range/4]).
 
 -export([init_with_opts/1, read_packet_from_buffer/1]).
@@ -26,6 +26,7 @@
     version,
     file,
     buffer,
+    buffer_end,
     stock,
     date,
     depth,
@@ -141,16 +142,77 @@ open_existing_db(FileName, FileOpts, GivenStockDBOpts) ->
 
   StateChunkRead = read_chunk_map(StateFileOpen),
 
-  StateReady = fast_forward(StateChunkRead),
+  StateReady = case lists:member(write, FileOpts) of
+    true -> fast_forward(StateChunkRead);
+    false -> StateChunkRead
+  end,
 
   % ?D({last_packet, StateReady#dbstate.last_timestamp}),
   {ok, StateReady}.
 
+restore_state(State) ->
+  {ok, fast_forward(State)}.
 
 update_db_options(OldOptions, _NewOptions) ->
   % TODO: decide what we can update
   OldOptions.
 
+buffer_data(#dbstate{} = State) ->
+  seek_utc(State#dbstate{buffer = undefined}, undefined).
+
+
+%% @doc Set buffer to contain data from given UTC timestamp to EOF
+seek_utc(#dbstate{file = File, chunk_map = ChunkMap, chunk_map_offset = ChunkMapOffset, chunk_size = ChunkSize,
+    buffer = OldBuffer, buffer_end = OldBufEnd} = State, UTC) ->
+
+  MinChunkStart = case UTC of
+    undefined -> 0;
+    Timestamp when is_integer(Timestamp) ->
+      Timestamp - timer:?CHUNKUNITS(ChunkSize)
+  end,
+
+  BufStart = case OldBuffer of
+    <<_/binary>> when is_integer(OldBufEnd) ->
+      OldBufEnd - erlang:byte_size(OldBuffer);
+    _ ->
+      undefined
+  end,
+
+  ChunksToRead = lists:dropwhile(fun({_N, T, _O}) -> T =< MinChunkStart end, ChunkMap),
+  {Buffer, BufEnd} = case ChunksToRead of
+    [] ->
+      {<<>>, undefined};
+    [{_N, _T, ROffset0}|_] when is_integer(BufStart) andalso BufStart =< ROffset0 ->
+      % No need to access file -- just trim existing buffer
+      SkipBytes = ROffset0 - BufStart,
+      <<_:SkipBytes/binary, NewBuffer/binary>> = OldBuffer,
+      {NewBuffer, OldBufEnd};
+    [{_N, _T, ROffset0}|_] ->
+      {ok, FileSize} = file:position(File, eof),
+      Offset0 = ROffset0 + ChunkMapOffset,
+      {ok, RealBuffer} = file:pread(File, Offset0, FileSize - Offset0),
+      {RealBuffer, FileSize - ChunkMapOffset}
+  end,
+  drop_before(State#dbstate{buffer = Buffer, buffer_end = BufEnd}, UTC).
+
+%% @doc Drop buffered packets until next one has later timestamp than given UTC
+drop_before(State, undefined) ->
+  State;
+drop_before(State, UTC) ->
+  case read_event(State) of
+    {eof, NextState} ->
+      % EOF. Just return what we have
+      NextState;
+    {Event, NextState} ->
+      case packet_timestamp(Event) of
+        Before when Before < UTC ->
+          % Drop more
+          drop_before(NextState, UTC);
+        _After ->
+          % Revert to state before getting event
+          State
+      end
+  end.
 
 read_file(FileName) ->
   {ok, State0} = open(FileName, [read, binary]),
@@ -206,7 +268,7 @@ foldl_range(Fun, Acc0, FileName, {Start, End}) ->
   MinChunkStart = case Start of
     undefined -> 0;
     Timestamp when is_integer(Timestamp) ->
-      Timestamp - ChunkSize
+      Timestamp - timer:?CHUNKUNITS(ChunkSize)
   end,
 
   File = State0#dbstate.file,
@@ -489,7 +551,7 @@ fast_forward(#dbstate{file = File, chunk_size = ChunkSize, chunk_map_offset = Ch
   {ok, FileSize} = file:position(File, eof),
 
   {ok, Buffer} = file:pread(File, AbsOffset, FileSize - AbsOffset),
-  LastState = case read_buffered_events(State#dbstate{buffer = Buffer, next_md_full = true}) of
+  LastState = case read_buffered_events(State#dbstate{buffer = Buffer, buffer_end = FileSize - ChunkMapOffset, next_md_full = true}) of
     {_Packets, OKState} ->
       OKState;
 
@@ -523,17 +585,32 @@ read_timestamp_at_offset(Offset, #dbstate{file = File, chunk_map_offset = ChunkM
 read_buffered_events(State) ->
   read_buffered_events(State, []).
 
-read_buffered_events(#dbstate{buffer = <<>>} = State, RevEvents) ->
-  {lists:reverse(RevEvents), State};
 read_buffered_events(#dbstate{} = State, RevEvents) ->
   try
-    {Event, NewState} = read_packet_from_buffer(State),
-    read_buffered_events(NewState, [Event|RevEvents])
+    case read_event(State) of
+      {eof, NewState} ->
+        {lists:reverse(RevEvents), NewState};
+      {Event, NewState} ->
+        read_buffered_events(NewState, [Event|RevEvents])
+    end
   catch
     error:Error ->
       ?D({parse_error, State#dbstate.buffer, Error, erlang:get_stacktrace()}),
       {parse_error, State, lists:reverse(RevEvents)}
   end.
+
+%% @doc Read first event in buffer and return it (or eof) with modified state
+read_event(#dbstate{buffer = undefined} = State) ->
+  % No buffer. Fill it and retry
+  read_event(buffer_data(State));
+
+read_event(#dbstate{buffer = <<>>} = State) ->
+  % Empty buffer. Return eof
+  {eof, State};
+
+read_event(State) ->
+  % Buffer seems to be OK, read packet
+  read_packet_from_buffer(State).
 
 read_packet_from_buffer(#dbstate{buffer = Buffer, depth = Depth, last_bidask = LastBidAsk, last_timestamp = LastTimestamp, scale = Scale} = State) ->
   case stockdb_format:packet_type(Buffer) of
