2a1eccca29b3b523f05e09a178663b7fc71f78de
Danil Zagoskin <z@gosk.in>
2012-08-10 18:51:58 +0400

    stockdb_raw: cleanup

--- src/stockdb_raw.erl
+++ src/stockdb_raw.erl
@@ -28,48 +28,9 @@ parse_opts([Unknown|MoreOpts], State) ->
   parse_opts(MoreOpts, State).
 
 
-is_stockdb_option({Option, _}) ->
-  lists:keymember(Option, 1, ?STOCKDB_OPTIONS);
-is_stockdb_option(_) -> false.
-
-
-utcdate() ->
-  {Date, _Time} = calendar:universal_time(),
-  Date.
-
 init_with_opts(Opts) ->
   parse_opts(Opts, #dbstate{}).
 
 
-
-update_db_options(OldOptions, _NewOptions) ->
-  % TODO: decide what we can update
-  OldOptions.
-
-
-
-%read_file(FileName) ->
-%  {ok, State0} = stockdb_reader:open(FileName),
-%  {ok, FileSize} = file:position(State0#dbstate.file, eof),
-%  [{_, ROffset0}|_] = stockdb_raw:nonzero_chunks(State0),
-%
-%  Offset0 = State0#dbstate.chunk_map_offset + ROffset0,
-%  {ok, Buffer} = file:pread(State0#dbstate.file, Offset0, FileSize - Offset0),
-%  close(State0),
-%
-%  {Events, _State1} = read_buffered_events(State0#dbstate{buffer = Buffer}),
-%  {ok, Events}.
-
-
 number_of_chunks(ChunkSize) ->
   timer:hours(24) div timer:seconds(ChunkSize) + 1.
-
-
-
-
-
-
-
-
- 
-
