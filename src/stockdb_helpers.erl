f787fb9bbfa422d118a454835a1ff8c4310ed832
Max Lapshin <max@maxidoors.ru>
2012-08-16 18:21:06 +0400

    fix in candle for day

--- src/stockdb_helpers.erl
+++ src/stockdb_helpers.erl
@@ -8,12 +8,15 @@
 -spec candle(stock(), date(), list(reader_option())) -> {price(),price(),price(),price()}.
 candle(Stock, Date, Options) ->
   {ok, Iterator} = stockdb:init_reader(Stock, Date, [{filter, candle, [{period, 24*3600*1000}]}|Options]),
-  Events = stockdb:events(Iterator),
-  Open = mid(lists:nth(1, Events)),
-  Close = mid(lists:nth(length(Events), Events)),
-  High = lists:max([mid(MD) || #md{} = MD <- Events]),
-  Low = lists:min([mid(MD) || #md{} = MD <- Events]),
+  % Events1 = stockdb:events(Stock, Date),
+  % Events = [mid(E) || E <- Events1, element(1,E) == trade],
+  Events = [mid(E) || E <- stockdb:events(Iterator)],
+  Open = lists:nth(1, Events),
+  Close = lists:nth(length(Events), Events),
+  High = lists:max(Events),
+  Low = lists:min(Events),
   {Open, High, Low, Close}.
 
 
+mid(#trade{price = Price}) -> Price;
 mid(#md{bid = [{Bid,_}|_], ask = [{Ask,_}|_]}) -> (Bid + Ask) / 2.
